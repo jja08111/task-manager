d4e0a28e068fc72ca766e7df474fdc1a59b7afb2
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2024-03-15 01:26:34 +0900

    Add requirements

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>작업 목록 관리자 앱</div>
+        <br />
+        <div>필수 요구사항</div>
+        <div>- 예정, 진행중, 완료됨 3개 상태</div>
+        <div>- 칸반 보드 형식 UI</div>
+        <div>- 완료 예정일 설정 기능</div>
+        <div>- 팀 단위, 실 단위 작업 상태 파악</div>
+        <br />
+        <div>추가 요구사항</div>
+        <div>- 드래그 드롭 기능</div>
       </header>
     </div>
   );
